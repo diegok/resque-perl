fa52fbff9a6d7af112e97a662c0abdf905746125
diegok <diego@freekeylabs.com>
2012-11-04 21:14:51 +0100

    Missing basic doc

--- lib/Resque/Plugin.pm
+++ lib/Resque/Plugin.pm
@@ -2,6 +2,8 @@ package Resque::Plugin;
 use Moose();
 use Moose::Exporter;
 
+# ABSTRACT: Syntactic sugar for Resque plugin's
+
 Moose::Exporter->setup_import_methods(
     with_meta => ['add_to'],
     also      => 'Moose',
